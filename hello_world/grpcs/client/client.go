27488d375b9c6628773758e3a443d7213ca56a43
wv <wvxiaobai@gamil.com>
2022-01-10 15:47:26 +0800

    first commit

--- hello_world/grpcs/client/client.go
+++ hello_world/grpcs/client/client.go
@@ -1,1 +1,24 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"go_rpc/hello_world/grpcs/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	c := hello.NewHelloClient(conn)
+	r, err := c.Hello(context.Background(), &hello.HelloRequest{Name: "wv"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(r.Reply)
+
+}
--- hello_world/grpcs/server/server.go
+++ hello_world/grpcs/server/server.go
@@ -1,1 +1,27 @@
-package server
+package main
+
+import (
+	"context"
+	"go_rpc/hello_world/grpcs/proto"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type Server struct {
+}
+
+func (s *Server) Hello(ctx context.Context, request *hello.HelloRequest) (*hello.HelloResponse, error) {
+	return &hello.HelloResponse{
+		Reply: "hello" + request.Name,
+	}, nil
+}
+
+func main() {
+	g := grpc.NewServer()
+	hello.RegisterHelloServer(g, &Server{})
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic("fail")
+	}
+	g.Serve(lis)
+}
